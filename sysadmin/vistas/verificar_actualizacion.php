8f52646fe7d79178c7a5471d1170e71e0f2a8cb3
Tics Empresariales <Tics Empresariales@Usuario>
2023-10-29 21:43:47 -0500

    Actualizacion Automatica

---
+++ sysadmin/vistas/verificar_actualizacion.php
@@ -0,0 +1,15 @@
+<?php
+//$repositoryPath = 'C:\xampp\htdocs\imporsuitv01'; // Reemplaza con la ruta de tu repositorio
+$command = "cd .. && git pull";
+
+$result = shell_exec($command);
+//echo strlen($result);
+// cd $repositoryPath && echo strlen($result);
+if (strlen($result)==20){
+ $actualzacion_sistema=0;   
+}else{
+ $actualzacion_sistema=1;  
+}
+?>
+
+
