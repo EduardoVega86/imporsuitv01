d90fc2b6aca015f017e23dfdd73db939f0455b9a
EduardoVega86 <edu.vega86@gmail.com>
2023-10-31 13:20:23 -0500

    Update generar_xml.php

--- sysadmin/vistas/xml/documentos/generar_xml.php
+++ sysadmin/vistas/xml/documentos/generar_xml.php
@@ -294,7 +294,7 @@ echo ' <script>obtenerComprobanteFirmado_sri("' . $ruta_certificado . '","' . $p
             $(window).on('load', function () {
                 setTimeout(function () {
                     $(".loader-page").css({visibility:"hidden",opacity:"0"})
-                }, 20000);
+                }, 100000);
                 
             });
         </script>
