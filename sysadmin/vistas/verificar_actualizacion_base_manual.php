125178850c5d2336d6e46b2f8e0a23d74475586c
USUARIO <USUARIO@DESKTOP-6A4H8NH>
2023-11-07 14:54:06 -0500

    buscar prodcutos

--- sysadmin/vistas/verificar_actualizacion_base_manual.php
+++ sysadmin/vistas/verificar_actualizacion_base_manual.php
@@ -6,6 +6,16 @@ require_once "db.php";
     
     // Consulta para verificar la existencia del campo
     
+//29-10-2023 aumentar campo id_articulo en testimonios
+$query = "SELECT id_producto FROM testimonios LIMIT 1";
+@$result = mysqli_fetch_array($query);
+if (!$result) {
+    
+    
+    $query = mysqli_query($conexion, "ALTER TABLE `testimonios` ADD `id_producto` INT NOT NULL AFTER `date_added`;");
+   
+} 
+
 
 ?>
 
