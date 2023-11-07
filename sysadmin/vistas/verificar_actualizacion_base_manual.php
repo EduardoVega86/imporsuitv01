01dc71fd496a35a475717bea6e7f41639bfb7e2a
USUARIO <USUARIO@DESKTOP-6A4H8NH>
2023-11-07 14:53:24 -0500

    buscar prodcutos

--- sysadmin/vistas/verificar_actualizacion_base_manual.php
+++ sysadmin/vistas/verificar_actualizacion_base_manual.php
@@ -1,5 +1,12 @@
 <?php
-echo 'pasa';
+//$repositoryPath = 'C:\xampp\htdocs\imporsuitv01'; // Reemplaza con la ruta de tu repositorio
+require_once "db.php";
+    require_once "php_conexion.php";
+   // require_once "sysadmin/vistas/funciones.php";
+    
+    // Consulta para verificar la existencia del campo
+    
+
 ?>
 
 
