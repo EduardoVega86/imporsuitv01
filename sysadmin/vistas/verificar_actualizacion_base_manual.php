28fb3b8ca73c015438cc18fb892d18597347c9bc
USUARIO <USUARIO@DESKTOP-6A4H8NH>
2023-11-10 14:57:23 -0500

    subir marketplace

--- sysadmin/vistas/verificar_actualizacion_base_manual.php
+++ sysadmin/vistas/verificar_actualizacion_base_manual.php
@@ -1,5 +1,5 @@
-ALTER TABLE `testimonios` ADD `id_producto` INT NOT NULL AFTER `date_added`;
-ALTER TABLE `perfil` ADD `habilitar_proveedor` INT NOT NULL AFTER `flotante`;
-ALTER TABLE `productos` ADD `tienda` VARCHAR(500) NOT NULL AFTER `valor4_producto`;
-ALTER TABLE `productos` ADD `drogshipin` INT NOT NULL DEFAULT '0' AFTER `tienda`;
+ALTER TABLE `testimonios` ADD `id_producto` INT  NULL AFTER `date_added`;
+ALTER TABLE `perfil` ADD `habilitar_proveedor` INT  NULL AFTER `flotante`;
+ALTER TABLE `productos` ADD `tienda` VARCHAR(500)  NULL AFTER `valor4_producto`;
+ALTER TABLE `productos` ADD `drogshipin` INT  NULL DEFAULT '0' AFTER `tienda`;
 ALTER TABLE `productos` ADD `id_producto_origen` INT NULL AFTER `drogshipin`; 
