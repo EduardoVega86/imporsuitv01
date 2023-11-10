256e5b03c15b4419cb0e53aab208a6e3d37454a8
USUARIO <USUARIO@DESKTOP-6A4H8NH>
2023-11-10 10:05:56 -0500

    arreglar_perfil

--- sysadmin/vistas/verificar_actualizacion_base_manual.php
+++ sysadmin/vistas/verificar_actualizacion_base_manual.php
@@ -1,35 +1,5 @@
-<?php
-//$repositoryPath = 'C:\xampp\htdocs\imporsuitv01'; // Reemplaza con la ruta de tu repositorio
-require_once "db.php";
-    require_once "php_conexion.php";
-   // require_once "sysadmin/vistas/funciones.php";
-    
-    // Consulta para verificar la existencia del campo
-    
-//29-10-2023 aumentar campo id_articulo en testimonios
-$query = "SELECT id_producto FROM testimonios LIMIT 1";
-$result = mysqli_query($conexion, $query);
- $result      = mysqli_num_rows($result);
-if (!$result) {
-    
-    
-    $query = mysqli_query($conexion, "ALTER TABLE `testimonios` ADD `id_producto` INT NOT NULL AFTER `date_added`;");
-   
-} 
-
-
-//29-10-2023 aumentar campo id_articulo en testimonios
-$query = "SELECT habilitar_proveedor FROM perfil LIMIT 1";
-$result = mysqli_query($conexion, $query);
- $result      = mysqli_num_rows($result);
-if (!$result) {
-    
-    
-     $query = mysqli_query($conexion, "ALTER TABLE `perfil` ADD `habilitar_proveedor` INT NOT NULL AFTER `flotante`;");
-   
-} 
-
-
-?>
-
-
+ALTER TABLE `testimonios` ADD `id_producto` INT NOT NULL AFTER `date_added`;
+ALTER TABLE `perfil` ADD `habilitar_proveedor` INT NOT NULL AFTER `flotante`;
+ALTER TABLE `productos` ADD `tienda` VARCHAR(500) NOT NULL AFTER `valor4_producto`;
+ALTER TABLE `productos` ADD `drogshipin` INT NOT NULL DEFAULT '0' AFTER `tienda`;
+ALTER TABLE `productos` ADD `id_producto_origen` INT NULL AFTER `drogshipin`; 
