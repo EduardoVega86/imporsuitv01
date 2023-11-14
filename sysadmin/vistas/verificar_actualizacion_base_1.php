1bc9165bd7357fcc372004f24300d12e99d21a02
USUARIO <USUARIO@DESKTOP-6A4H8NH>
2023-11-13 20:09:30 -0500

    subir marketplace

--- sysadmin/vistas/verificar_actualizacion_base_1.php
+++ sysadmin/vistas/verificar_actualizacion_base_1.php
@@ -1,7 +1,7 @@
 <?php
 //$repositoryPath = 'C:\xampp\htdocs\imporsuitv01'; // Reemplaza con la ruta de tu repositorio
 require_once "db.php";
-    require_once "php_conexion.php";
+require_once "php_conexion.php";
    // require_once "sysadmin/vistas/funciones.php";
     
     // Consulta para verificar la existencia del campo
