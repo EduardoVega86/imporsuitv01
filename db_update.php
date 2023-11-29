2861f58b809de6f737fbb9435e3702149e161379
USUARIO <USUARIO@DESKTOP-6A4H8NH>
2023-11-29 00:33:43 -0500

    pedido multitienda

--- db_update.php
+++ db_update.php
@@ -875,8 +875,9 @@ mysqli_query($conexion, "ALTER TABLE `facturas_cot` ADD `identificacion` VARCHAR
 
 mysqli_query($conexion, "ALTER TABLE `users` ADD `token_act` VARCHAR(400) NULL AFTER `comision_users`, ADD `estado_token` INT NULL AFTER `token_act`, ADD `fecha_actualizacion` DATETIME NULL AFTER `estado_token`");
 
-mysqli_query($conexion, "ALTER TABLE `detalle_fact_cot` ADD `drogshipin` INT NULL AFTER `precio_venta`");
-
+mysqli_query($conexion, "ALTER TABLE `tmp_ventas` ADD `drogshipin_tmp` INT NULL AFTER `session_id`");
+mysqli_query($conexion, "ALTER TABLE `facturas_cot` ADD `drogshipin` INT NULL AFTER `valor_seguro`");
+mysqli_query($conexion, "ALTER TABLE `facturas_cot` ADD `tienda` VARCHAR(500) NULL AFTER `drogshipin`");
  
 mysqli_close($conexion); // Cerramos la link con la base de datos
 
