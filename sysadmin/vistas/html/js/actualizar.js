911284e58a6aa2dd8cdc7e0db18bf9b72c9bb866
Jeimy Jara Desarrollador 1 <desarrollador1@imporfactoryusa.com>
2023-11-30 16:30:35 -0500

    Actualizar fixed

--- sysadmin/vistas/html/js/actualizar.js
+++ sysadmin/vistas/html/js/actualizar.js
@@ -25,23 +25,24 @@ const actualizar = () => {
             title: "¡Actualización del sistema exitosa!",
             icon: "success",
             showConfirmButton: false,
-            timer: 1000,
-          }).then(async () => {
-            const responseAS = await fetch(dominio + "db_update291123.php", {
-              method: "GET",
-              headers: {
-                "Content-Type": "application/json",
-              },
-            });
-            responseAS.json().then((data) => {
-              if (data === "ok") {
-                Swal.fire({
-                  title: "¡Actualización de la base de datos exitosa!",
-                  icon: "success",
-                  confirmButtonText: "¡Aceptar!",
-                });
-              }
-            });
+            showLoaderOnConfirm: true,
+            preConfirm: async () => {
+              const responseAS = await fetch(dominio + "db_update291123.php", {
+                method: "GET",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              });
+              responseAS.json().then((data) => {
+                if (data === "ok") {
+                  Swal.fire({
+                    title: "¡Actualización de la base de datos exitosa!",
+                    icon: "success",
+                    confirmButtonText: "¡Aceptar!",
+                  });
+                }
+              });
+            },
           });
         }
       });
