56b71139ab3341cc29c5dd603f369dcd3a30fda4
Jeimy Jara Desarrollador 1 <desarrollador1@imporfactoryusa.com>
2023-12-01 16:00:35 -0500

    Loading actualizar

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ include 'includes/head.php'
                            if (empty(get_row('perfil', 'logo_url', 'id_perfil', '1'))) {
                               echo "assets/img/image.png";
                            } else {
-                              echo  get_row('perfil', 'logo_url', 'id_perfil', '1');
+                              echo str_replace("../..", "", get_row('perfil', 'logo_url', 'id_perfil', '1'));
                            }
                            ?>" alt="Imagen" />
 
