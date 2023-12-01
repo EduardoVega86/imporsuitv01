02afe2b0d049fa643aa1b356037c4d86a64c9356
Jeimy Jara Desarrollador 1 <desarrollador1@imporfactoryusa.com>
2023-12-01 10:45:32 -0500

    db fix 6

--- sysadmin/vistas/db.php
+++ sysadmin/vistas/db.php
@@ -13,20 +13,16 @@ if (count($subdominioComponentes) > 2) {
     $primeraParte = $subdominioComponentes[0];
 }
 $user = "imporsuit_" . $primeraParte;
+$pass = "imporsuit_" . $primeraParte;
 @define('DB_HOST', 'localhost'); //DB_HOST:  generalmente suele ser "127.0.0.1"
 @define('DB_USER', $user); //Usuario de tu base de datos
 if ($url == 'imporshop.imporsuit.com') {
-    @define('DB_PASS', 'E?c7Iij&885Y'); //Contraseña del usuario de la base de datos
-} else {
-    @define('DB_PASS', $user); //Contraseña del usuario de la base de datos
+    $pass = 'E?c7Iij&885Y';
 }
+@define('DB_PASS', $pass); //Contraseña del usuario de la base de datos
 @define('DB_NAME', $user); //Nombre de la base de datos
 
 $host = 'localhost';
 $usuario = $user;
-if ($url == 'imporshop.imporsuit.com') {
-    $contrasena = 'E?c7Iij&885Y';
-} else {
-    $contrasena = $user;
-}
+$contrasena = $pass;
 $base_de_datos = $user;
