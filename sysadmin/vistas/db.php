0fbbe5ca99e83720e6fcba3b5300ed719af09791
Jeimy Jara Desarrollador 1 <desarrollador1@imporfactoryusa.com>
2023-12-01 08:41:57 -0500

    db2

--- sysadmin/vistas/db.php
+++ sysadmin/vistas/db.php
@@ -1,12 +1,12 @@
 <?php
 //echo 'asd';
 @define('DB_HOST', 'localhost'); //DB_HOST:  generalmente suele ser "127.0.0.1"
-@define('DB_USER', 'imporsuit_alvitorsa'); //Usuario de tu base de datos
-@define('DB_PASS', 'imporsuit_alvitorsa'); //Contrasena del usuario de la base de datos
+@define('DB_USER', 'root'); //Usuario de tu base de datos
+@define('DB_PASS', ''); //Contrasena del usuario de la base de datos
 @define('DB_NAME', 'imporsuit_alvitorsa'); //Nombre de la base de datos
 
 
 $host = 'localhost';
-$usuario = 'imporsuit_alvitorsa';
-$contrasena = 'imporsuit_alvitorsa';
-$base_de_datos = 'prueba_imporsuit';
+$usuario = 'root';
+$contrasena = '';
+$base_de_datos = 'imporsuit_alvitorsa';
