f99835afb5144bae9182c6e999fcafd79c84aec0
DesarrolloImporfactory <152187747+DesarrolloImporfactory@users.noreply.github.com>
2023-12-01 19:09:55 -0500

    Update db.php

--- sysadmin/vistas/db.php
+++ sysadmin/vistas/db.php
@@ -1,35 +1,12 @@
 <?php
-
-$url = $_SERVER['HTTP_HOST'];
-
-$parsedUrl = parse_url($url);
-// Obtiene el fragmento que necesitas
-$primeraParte = $parsedUrl['path'];
-// Verifica si es un subdominio
-$subdominioComponentes = explode('.', $primeraParte);
-if (count($subdominioComponentes) > 2) {
-    // Si es un subdominio, obtenemos el subdominio
-    $primeraParte = $subdominioComponentes[0];
-} else {
-    // Si no es un subdominio, obtenemos el dominio
-    $primeraParte = $subdominioComponentes[0];
-}
-
-
-$user = "imporsuit_" . $primeraParte;
-$pass = "imporsuit_" . $primeraParte;
-
-
-
+//echo 'asd';
 @define('DB_HOST', 'localhost'); //DB_HOST:  generalmente suele ser "127.0.0.1"
-@define('DB_USER', $user); //Usuario de tu base de datos
-if ($user == 'imporsuit_imporshop') {
-    $pass = 'E?c7Iij&885Y';
-}
-@define('DB_PASS', $pass); //Contraseña del usuario de la base de datos
-@define('DB_NAME', $user); //Nombre de la base de datos
+@define('DB_USER', 'root'); //Usuario de tu base de datos
+@define('DB_PASS', ''); //Contrasena del usuario de la base de datos
+@define('DB_NAME', 'prueba_imporsuit'); //Nombre de la base de datos
+
 
 $host = 'localhost';
-$usuario = $user;
-$contrasena = $pass;
-$base_de_datos = $user;
+$usuario = 'root';
+$contrasena= '';
+$base_de_datos = 'prueba_imporsuit';
