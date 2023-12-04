0b07e9a4cee29aadb36a6e27053c00d3252948be
Jeimy Jara Desarrollador 1 <desarrollador1@imporfactoryusa.com>
2023-12-04 11:24:04 -0500

    DB rollback

--- sysadmin/vistas/db.php
+++ sysadmin/vistas/db.php
@@ -1,12 +1,10 @@
 <?php
 @define('DB_HOST', 'localhost'); //DB_HOST:  generalmente suele ser "127.0.0.1"
-@define('DB_USER', 'root'); //Usuario de tu base de datos
-@define('DB_PASS', ''); //Contrasena del usuario de la base de datos
-@define('DB_NAME', 'prueba_imporsuit'); //Nombre de la base de datos
-
+@define('DB_USER', 'imporsuit_alvitorsa'); //Usuario de tu base de datos
+@define('DB_PASS', 'imporsuit_alvitorsa'); //Contrasena del usuario de la base de datos
+@define('DB_NAME', 'imporsuit_alvitorsa'); //Nombre de la base de datos
 
 $host = 'localhost';
-$usuario = 'root';
-$contrasena= '';
-$base_de_datos = 'prueba_imporsuit';
-
+$usuario = 'imporsuit_alvitorsa';
+$contrasena = 'imporsuit_alvitorsa';
+$base_de_datos = 'imporsuit_alvitorsa';
