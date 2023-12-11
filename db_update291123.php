970a74be0822bd7c5b3c25761f752a1aed0e1538
Jeimy Jara Desarrollador 1 <desarrollador1@imporfactoryusa.com>
2023-12-11 09:11:19 -0500

    Peso colombiano y sol peruano añadido

--- db_update291123.php
+++ db_update291123.php
@@ -951,7 +951,7 @@ mysqli_query($conexion, "INSERT INTO estado_courier (codigo, id_servicio, descri
 mysqli_query($conexion, "INSERT INTO estado_courier (codigo, id_servicio, descripcion, alias) VALUES (48, '1', 'Registro de Novedad de COD x Inventario', 'COD')");
 mysqli_query($conexion, "INSERT INTO estado_courier (codigo, id_servicio, descripcion, alias) VALUES (49, '1', 'Eliminacion COD', 'COD')");
 mysqli_query($conexion, "INSERT INTO estado_courier (codigo, id_servicio, descripcion, alias) VALUES (50, '1', 'Actualizacion Valor COD', 'COD')");
-mysqli_query($conexion, "INSERT INTO `currencies` (`id`, `name`, `symbol`, `precision`, `thousand_separator`, `decimal_separator`, `code`) VALUES (32, 'Sol', 'S/.', '2', ',', '.', 'PEN')");
+mysqli_query($conexion, "INSERT INTO `currencies` (`id`, `name`, `symbol`, `precision`, `thousand_separator`, `decimal_separator`, `code`) VALUES (33, 'Sol', 'S/.', '2', ',', '.', 'PEN')");
 mysqli_query($conexion, "ALTER TABLE `facturas_cot` ADD `importado` INT NULL DEFAULT '0' AFTER `tienda`");
 mysqli_query($conexion, "ALTER TABLE `facturas_cot` ADD `plataforma_importa` VARCHAR(500) NULL  AFTER `importado`");
 mysqli_query($conexion, "CREATE TABLE `estado_guia_sistema` (
