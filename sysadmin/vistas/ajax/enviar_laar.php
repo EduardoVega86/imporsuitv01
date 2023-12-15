e57b9ccb1a11ef85cba9346822ce77e68a6fa62e
USUARIO <USUARIO@DESKTOP-6A4H8NH>
2023-12-15 13:56:45 -0500

    ARRGLAR GTRACIASddd

--- sysadmin/vistas/ajax/enviar_laar.php
+++ sysadmin/vistas/ajax/enviar_laar.php
@@ -206,7 +206,8 @@ $destino_url = "https://api.laarcourier.com:9727/guias/contado";
 // Datos a enviar en formato JSON al servicio de destino
 $cantidad_total_prducto = $cantidad_total;
 $cantidad_total = 1;
-$productos_guia = $cantidad_total_prducto . ' ' . $productos_guia;
+$productos_guia =  $productos_guia;
+$observacion = $observacion.'-Por:'.$server_url;
 $datos_destino = array(
     "origen" => array(
         "identificacionO" => "$identificacionO",
