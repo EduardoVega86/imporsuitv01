3ee622c26c3083ef17e2fda78f709a749c7e989b
Jeimy Jara Desarrollador 1 <desarrollador1@imporfactoryusa.com>
2023-12-18 11:40:52 -0500

    no enviados

---
+++ sysadmin/js/bitacora_noenviado.js
@@ -0,0 +1,90 @@
+$(document).ready(function () {
+  load(1);
+});
+$("#editar_linea").submit(function (event) {
+  // alert();
+  $("#actualizar_datos").attr("disabled", true);
+  var parametros = $(this).serialize();
+  $.ajax({
+    type: "POST",
+    url: "../ajax/editar_pedido.php",
+    data: parametros,
+    beforeSend: function (objeto) {
+      $("#resultados_ajax2").html(
+        '<img src="../../img/ajax-loader.gif"> Cargando...'
+      );
+    },
+    success: function (datos) {
+      $("#resultados_ajax2").html(datos);
+      $("#actualizar_datos").attr("disabled", false);
+      //load(1);
+      $("#editarLinea").modal("hide");
+
+      window.setTimeout(function () {
+        $(".alert")
+          .fadeTo(200, 0)
+          .slideUp(200, function () {
+            $(this).remove();
+          });
+      }, 2000);
+    },
+  });
+  event.preventDefault();
+});
+function load(page) {
+  var q = $("#q").val();
+  var tienda = $("#tienda_q").val();
+  var url = "";
+
+  if (tienda != 0) {
+    url =
+      "../ajax/buscar_noenviado.php?action=ajax&page=" +
+      page +
+      "&q=" +
+      q +
+      "&tienda=" +
+      tienda;
+  } else {
+    url = "../ajax/buscar_noenviado.php?action=ajax&page=" + page + "&q=" + q;
+  }
+  $("#loader").fadeIn("slow");
+  $.ajax({
+    url: url,
+    beforeSend: function (objeto) {
+      $("#loader").html('<img src="../../img/ajax-loader.gif"> Cargando...');
+    },
+    success: function (data) {
+      $(".outer_div").html(data).fadeIn("slow");
+      $("#loader").html("");
+      $('[data-toggle="tooltip"]').tooltip({
+        html: true,
+      });
+    },
+  });
+}
+
+function buscar(tienda) {
+  // alert(tienda)
+  var q = $("#q").val();
+  page = 1;
+  $("#loader").fadeIn("slow");
+  $.ajax({
+    url:
+      "../ajax/buscar_noenviado.php?action=ajax&page=" +
+      page +
+      "&tienda=" +
+      tienda +
+      "&q=" +
+      q,
+    beforeSend: function (objeto) {
+      $("#loader").html('<img src="../../img/ajax-loader.gif"> Cargando...');
+    },
+    success: function (data) {
+      $(".outer_div").html(data).fadeIn("slow");
+      $("#loader").html("");
+      $('[data-toggle="tooltip"]').tooltip({
+        html: true,
+      });
+    },
+  });
+}
