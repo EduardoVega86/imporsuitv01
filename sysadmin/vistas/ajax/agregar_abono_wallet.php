78c6d26c4fb882bb63ad171f3b5a6e1fa95339ba
Jeimy Jara Desarrollador 1 <desarrollador1@imporfactoryusa.com>
2023-12-29 10:41:13 -0500

    Wallet fixed

--- sysadmin/vistas/ajax/agregar_abono_wallet.php
+++ sysadmin/vistas/ajax/agregar_abono_wallet.php
@@ -1,24 +1,21 @@
 <?php
-include 'is_logged.php'; //Archivo comprueba si el usuario esta logueado
+include 'is_logged.php';
 
 $tienda = $_SESSION['tienda'];
-
-/* Connect To Database*/
 require_once "../db.php";
+
 if (empty($_POST['abono'])) {
     $errors[] = "Cantidad vacía";
-} else if (!empty($_POST['abono'])) {
-    /* Connect To Database*/
-    require_once "../db.php";
+} elseif (!empty($_POST['abono'])) {
     require_once "../php_conexion.php";
     require_once "../funciones.php";
-    // escaping, additionally removing everything that could be (html/javascript-) code
-    $abono    = floatval($_POST['abono']);
+
+    $abono = floatval($_POST['abono']);
     $total_abonado = $abono;
     $forma_pago = $_POST["forma_pago"];
 
     $imagen = $_FILES['img']['name'];
-    $ruta   = $_FILES['img']['tmp_name'];
+    $ruta = $_FILES['img']['tmp_name'];
 
     $nombre_alterado = date("Y-m-d-H-i-s") . "-" . $imagen;
     $url_ubicacion = "https://marketplace.imporsuit.com/sysadmin/img/facturas/" . $nombre_alterado;
@@ -26,47 +23,23 @@ if (empty($_POST['abono'])) {
     $destino = "../../img/facturas/" . $nombre_alterado;
     copy($ruta, $destino);
 
-
     $user_id  = $_SESSION['id_users'];
     $fecha    = date("Y-m-d H:i:s");
-    // Consulta para Extraer los datos del credito
 
-    $consultar     = mysqli_query($conexion, "SELECT * FROM `cabecera_cuenta_pagar` where tienda ='$tienda' AND `valor_pendiente` != 0;");
-    $rw            = mysqli_fetch_array($consultar);
+    $consultar = mysqli_query($conexion, "SELECT * FROM `cabecera_cuenta_pagar` WHERE tienda ='$tienda' AND `valor_pendiente` != 0 AND visto ='1' ORDER by monto_recibir ASC;");
 
-    $total_monto_recibir_sql = "SELECT SUM(subquery.total_venta) as total_ventas, SUM(subquery.total_pendiente) as total_pendiente FROM ( SELECT numero_factura, MAX(total_venta) as total_venta, MAX(valor_pendiente) as total_pendiente FROM cabecera_cuenta_pagar WHERE tienda = '$tienda' GROUP BY numero_factura ) as subquery;";
-    $total_monto_recibir_query = mysqli_query($conexion, $total_monto_recibir_sql);
-    $total_monto_recibir_SQL = mysqli_fetch_array($total_monto_recibir_query);
-
-    $total_monto_recibir = $total_monto_recibir_SQL['total_pendiente'];
-    $total_monto_recibir = number_format($total_monto_recibir, 2, '.', '');
-    // verificamos si el monto esta cancelado
-    if ($rw['monto_recibir'] == 0) {
-        echo "<script>
-        $.Notification.notify('info','bottom center','NOTIFICACIÓN', 'LA FACTURA YA FUE CANCELADA EN SU TOTALIDAD')
-        </script>";
-        exit;
-    }
-    // verificamos si el abono es mayor a la deunda
-    if ($abono > $total_monto_recibir) {
-        echo "<script>
-        $.Notification.notify('error','bottom center','NOTIFICACIÓN', 'EL ABONO ES MAYOR A LA DEUDA, INTENTAR NUEVAMENTE')
-        </script>";
-        exit;
-    }
-    $i = 0;
-    foreach ($consultar as $rws) {
-
+    while ($rws = mysqli_fetch_assoc($consultar)) {
         if ($abono > 0) {
-
             $bct = 0;
-            $saldo = $rws['monto_recibir'] - $abono;
+
+            $saldo = $rws['valor_pendiente'] - $abono;
+
             if ($saldo < 0) {
-                $abono =  -$saldo;
+                $abono = -$saldo;
                 $bct = $rws['monto_recibir'];
                 $saldo = 0;
-                $cobrado = $rws['total_venta'] - $rws['costo'] - $rws['precio_envio'];
-            } else if ($saldo == 0) {
+                $cobrado = $rws['monto_recibir'];
+            } elseif ($saldo == 0) {
                 $cobrado = $rws['total_venta'] - $rws['costo'] - $rws['precio_envio'];
                 $bct = $abono;
                 $abono = 0;
@@ -75,44 +48,46 @@ if (empty($_POST['abono'])) {
                 $bct = $abono;
                 $abono = 0;
             }
-            $numero_factura = $rws['numero_factura'];
-            // Se actualiza la primera factura que se encuentra en la base de datos
-            $sql_update = "UPDATE `cabecera_cuenta_pagar` SET `monto_recibir`='$saldo',`valor_cobrado`='$cobrado', valor_pendiente='$saldo' WHERE `numero_factura`='$numero_factura';";
 
+
+            $numero_factura = $rws['numero_factura'];
+
+            $sql_update = "UPDATE `cabecera_cuenta_pagar` SET `valor_cobrado`='$cobrado', valor_pendiente='$saldo' WHERE `numero_factura`='$numero_factura';";
             $query_update = mysqli_query($conexion, $sql_update);
+
             if ($i == 0) {
-                $sql_pago = "INSERT INTO `pagos`( `fecha`, `numero_documento`, `valor`, `forma_pago`,`tienda`,`imagen`  ) VALUES ('$fecha', '$numero_factura', $total_abonado, '$forma_pago', '$tienda', '$url_ubicacion');";
+                $sql_pago = "INSERT INTO `pagos`(`fecha`, `numero_documento`, `valor`, `forma_pago`, `tienda`, `imagen`) VALUES ('$fecha', '$numero_factura', $total_abonado, '$forma_pago', '$tienda', '$url_ubicacion');";
                 $query_pago = mysqli_query($conexion, $sql_pago);
             }
+
             $i++;
 
             $sql_detalle_pago = "INSERT INTO `detalle_cuenta_pagar`(`valor`, `id_cabecera_cpp`, `signo`, `metodo_pago`, `id_pago`) VALUES " .
-                "($bct, (SELECT MAX( id_cabecera) FROM `cabecera_cuenta_pagar` WHERE numero_factura = '$numero_factura'), '+', '$forma_pago', (SELECT MAX(id_pago) FROM `pagos`));";
+                "($bct, (SELECT MAX(id_cabecera) FROM `cabecera_cuenta_pagar` WHERE numero_factura = '$numero_factura'), '+', '$forma_pago', (SELECT MAX(id_pago) FROM `pagos`));";
             $query_detalle_pago = mysqli_query($conexion, $sql_detalle_pago);
 
-            $comprobar = mysqli_query($conexion, "select * from cabecera_cuenta_pagar where numero_factura='" . $numero_factura . "'");
-            $rww       = mysqli_fetch_array($comprobar);
+            $comprobar = mysqli_query($conexion, "SELECT * FROM cabecera_cuenta_pagar WHERE numero_factura='$numero_factura'");
+            $rww = mysqli_fetch_array($comprobar);
+
             if ($rww['monto_recibir'] == 0) {
-                $up_credito = mysqli_query($conexion, "update cabecera_cuenta_pagar set estado_factura=1 where numero_factura='$numero_factura'");
+                $up_credito = mysqli_query($conexion, "UPDATE cabecera_cuenta_pagar SET estado_factura=1 WHERE numero_factura='$numero_factura'");
                 echo "<script>
                 $.Notification.notify('info','bottom center','NOTIFICACIÓN', 'LA FACTURA SE HA CANCELADO EN SU TOTALIDAD')
                 </script>";
             }
+
             if ($rww) {
-                $messages[] = "El Abono  ha sido ingresado satisfactoriamente." . '' . $numero_factura;
+                $messages[] = "El Abono ha sido ingresado satisfactoriamente. $numero_factura";
             } else {
-                $errors[] = "Lo siento algo ha salido mal intenta nuevamente." . mysqli_error($conexion);
+                $errors[] = "Lo siento, algo ha salido mal. Intenta nuevamente. " . mysqli_error($conexion);
             }
         } else {
             break;
         }
     }
-    // guardamos los datos la tabla de abonos
-
 } else {
     $errors[] = "Error desconocido.";
 }
-
 
 if (isset($errors)) {
 ?>
@@ -127,6 +102,7 @@ if (isset($errors)) {
     </div>
 <?php
 }
+
 if (isset($messages)) {
 ?>
     <div class="alert alert-success" role="alert">
--- sysadmin/vistas/ajax/cargar_widget_wallet.php
+++ sysadmin/vistas/ajax/cargar_widget_wallet.php
@@ -12,7 +12,7 @@ include "../permisos.php";
 //Archivo de funciones PHP
 require_once "../funciones.php";
 $simbolo_moneda = get_row('perfil', 'moneda', 'id_perfil', 1);
-$valor_total_tienda_sql = "SELECT SUM(subquery.total_venta) as total_ventas, SUM(subquery.total_pendiente) as total_pendiente, SUM(subquery.total_cobrado) as total_cobrado FROM ( SELECT numero_factura, MAX(total_venta) as total_venta, MAX(valor_pendiente) as total_pendiente, MAX(valor_cobrado) as total_cobrado FROM cabecera_cuenta_pagar WHERE tienda = '$tienda' AND visto = '1' GROUP BY numero_factura ) as subquery;";
+$valor_total_tienda_sql = "SELECT SUM(subquery.total_venta) as total_ventas, SUM(subquery.total_pendiente) as total_pendiente, SUM(subquery.total_cobrado) as total_cobrado, SUM(subquery.monto_recibir) as monto_recibir FROM ( SELECT numero_factura, MAX(total_venta) as total_venta, MAX(valor_pendiente) as total_pendiente, MAX(valor_cobrado) as total_cobrado, MAX(monto_recibir) as monto_recibir FROM cabecera_cuenta_pagar WHERE tienda = '$tienda' AND visto = '1' GROUP BY numero_factura ) as subquery;";
 $valor_total_tienda_query = mysqli_query($conexion, $valor_total_tienda_sql);
 
 $valor_total_tienda_SQL = mysqli_fetch_array($valor_total_tienda_query);
@@ -20,7 +20,7 @@ $valor_total_tienda_SQL = mysqli_fetch_array($valor_total_tienda_query);
 $valor_total_tienda = $valor_total_tienda_SQL['total_ventas'];
 $valor_total_pendiente = $valor_total_tienda_SQL['total_pendiente'];
 $valor_total_cobrado = $valor_total_tienda_SQL['total_cobrado'];
-
+$valor_total_monto_recibir = $valor_total_tienda_SQL['monto_recibir'];
 
 ?>
 <div class="col-lg-12 col-md-6">
@@ -34,6 +34,19 @@ $valor_total_cobrado = $valor_total_tienda_SQL['total_cobrado'];
         </div>
     </div>
 </div>
+
+<div class="col-lg-12 col-md-6">
+    <div class="card-box widget-icon">
+        <div>
+            <i class="mdi mdi-briefcase-check text-primary"></i>
+            <div class="wid-icon-info text-right">
+                <p class="text-muted m-b-5 font-13 font-bold text-uppercase">Utilidad Generada</p>
+                <h4 class="m-t-0 m-b-5 counter font-bold text-primary"><?php echo $simbolo_moneda . '' . number_format($valor_total_monto_recibir, 2); ?></h4>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="col-lg-12 col-md-6">
     <div class="card-box widget-icon">
         <div>
