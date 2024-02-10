50422d75fbba7d084a98aeffeb6af21c6cfc1221
USUARIO <USUARIO@DESKTOP-6A4H8NH>
2024-02-09 22:39:35 -0500

    guia_local

--- sysadmin/vistas/ajax/incrementa_cod_prod.php
+++ sysadmin/vistas/ajax/incrementa_cod_prod.php
@@ -6,7 +6,7 @@ include 'is_logged.php'; //Archivo verifica que el usario que intenta acceder a 
 require_once "../db.php";
 require_once "../php_conexion.php";
 $query_id = mysqli_query($conexion, "SELECT RIGHT(codigo_producto,6) as codigo FROM productos
-  ORDER BY codigo_producto DESC LIMIT 1")
+  ORDER BY id_producto DESC LIMIT 1")
   or die('error ' . mysqli_error($conexion));
 $count = mysqli_num_rows($query_id);
 
