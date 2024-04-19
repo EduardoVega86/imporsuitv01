f7eb2d99f5e566d0484b9632b66449ddfa429780
User <User@DESKTOP-9FVJ3SL>
2024-04-19 15:24:07 -0500

    MARKETPLACE

--- db_update291123.php
+++ db_update291123.php
@@ -2268,7 +2268,10 @@ mysqli_query($conexion, "CREATE TABLE novedades (
 );");
 
 mysqli_query($conexion, "ALTER TABLE `facturas_ventas` DROP INDEX `numero_cotizacion`;");
- 
+mysqli_query($conexion, "ALTER TABLE `detalle_fact_ventas` ADD `descripcion_detalle` TEXT NOT NULL AFTER `importe_venta`;");
+mysqli_query($conexion, "ALTER TABLE `detalle_fact_ventas` ADD `aplica_iva` INT NOT NULL AFTER `descripcion_detalle`;");
+
+  
  
 mysqli_close($conexion); // Cerramos la link con la base de datos
 
