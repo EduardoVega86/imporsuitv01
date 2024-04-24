ae63539c1992f1b92ef7725a76039258cef30f9b
User <User@DESKTOP-9FVJ3SL>
2024-04-24 10:16:38 -0500

    MARKETPLACE

--- sysadmin/vistas/ajax/carga_num_trans.php
+++ sysadmin/vistas/ajax/carga_num_trans.php
@@ -8,15 +8,17 @@ $query_id = mysqli_query($conexion, "SELECT RIGHT(num_trans_factura,6) as trans 
 or die('error ' . mysqli_error($conexion));
 $count = mysqli_num_rows($query_id);
 
-if ($count != 0) {
+if (@$count != 0) {
 
     $rw    = mysqli_fetch_assoc($query_id);
-    $trans = $rw['trans'] + 1;
+    @$trans = $rw['trans'] + 1;
+    //echo $trans;
 } else {
     $trans = 1;
 }
 
 $buat_id = str_pad($trans, 6, "0", STR_PAD_LEFT);
 $trans   = "T$buat_id";
+echo $trans;
 
-echo '<input type="hidden" class="form-control input-sm" autocomplete="off" id="trans" value="' . $trans . '" placeholder="trans" readonly name="trans" >';
+//echo '<input type="text" class="form-control input-sm" autocomplete="off" id="trans1" value="' . $trans . '" placeholder="trans" name="trans1" >';
