45f76e7ce96097e627b9fc4874d27f811c00f154
Jeimy Jara <jeimyjara@hotmail.com>
2024-04-29 13:29:59 -0500

    update

--- sysadmin/vistas/ajax/consultar_guia_laar.php
+++ sysadmin/vistas/ajax/consultar_guia_laar.php
@@ -8,24 +8,55 @@ $guia = $_POST['guia'];
 
 $consulta = "SELECT * FROM guia_laar WHERE guia_laar = '$guia'";
 $resultado = mysqli_query($conexion, $consulta);
+$guia = mysqli_fetch_assoc($resultado);
 
-if (mysqli_num_rows($resultado) > 0) {
-    $fila = mysqli_fetch_array($resultado);
-    $respuesta = array(
-        'guia' => $fila['guia_laar'],
-        'fecha' => $fila['fecha'],
-        'hora' => $fila['hora'],
-        'destino' => $fila['destino'],
-        'observaciones' => $fila['observaciones'],
-        'estado' => $fila['estado']
-    );
+$ciudadDestino = $guia['ciudadD'];
+$id_pedido = $guia['id_pedido'];
+
+$sql = "SELECT * FROM ciudad_cotizacion WHERE codigo_ciudad_laar = '$ciudadDestino'";
+$result = mysqli_query($conexion, $sql);
+$ciudad = mysqli_fetch_assoc($result);
+if ($ciudad) {
+    $ciudadDestino = $ciudad['ciudad'];
 } else {
-    $respuesta = array(
-        'guia' => '',
-        'fecha' => '',
-        'hora' => '',
-        'destino' => '',
-        'observaciones' => '',
-        'estado' => ''
-    );
+    $ciudadDestino = '';
 }
+
+
+$sql = "SELECT * FROM facturas_cot where id_factura = '$id_pedido'";
+$result = mysqli_query($conexion, $sql);
+$factura = mysqli_fetch_assoc($result);
+
+$nombre = $factura['nombre'];
+$ciudad = $factura['ciudad_cot'];
+$telefono = $factura['telefono'];
+$celular = $factura['celular'];
+$c_prinicpal = $factura['c_principal'];
+$c_secundaria = $factura['c_secundaria'];
+$numeracion = 0;
+$referencia = $factura['referencia'];
+$observacion = $factura['observacion'];
+
+
+$datos = array(
+    'guia' => $guia['guia_laar'],
+    'destino' => array(
+        'ciudad' => $ciudadDestino,
+        'nombre' => $nombre,
+        'ciudad' => $ciudad,
+        'telefono' => $telefono,
+        'celular' => $celular,
+        'c_principal' => $c_prinicpal,
+        'c_secundaria' => $c_secundaria,
+        'numeracion' => $numeracion,
+        'referencia' => $referencia,
+        'observacion' => $observacion
+    ),
+    'autorizado' => array(
+        'nombre' => "IMPORTSUIT",
+        'isDevolucion' => false
+    )
+);
+
+
+echo json_encode($datos);
