08ed74ede08d2e904bf92784fb2113256f4053e0
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-05-23 16:52:45 -0500

    filtro estado servientrega

--- sysadmin/vistas/ajax/buscar_cotizacion_new.php
+++ sysadmin/vistas/ajax/buscar_cotizacion_new.php
@@ -1084,7 +1084,7 @@ if ($action == 'ajax' && ($server_url == "https://marketplace.imporsuit.com")) {
     $reload      = '../reportes/facturas.php';
     //main query to fetch the data
     $sql   = "SELECT * FROM  $sTable $sWhere LIMIT $offset,$per_page";
-    echo $sql;
+    //echo $sql;
     $query = mysqli_query($conexion, $sql);
     $empresas = mysqli_query($conexion, "SELECT * FROM trabajadores_envio where estado=1");
     //loop through fetched data0
