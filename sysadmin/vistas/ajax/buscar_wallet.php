d5d0eddb0a5949e18019b4bc97f956dc68a6f091
Jeimy Jara <jeimyjara@hotmail.com>
2024-05-30 19:42:11 -0500

    update

--- sysadmin/vistas/ajax/buscar_wallet.php
+++ sysadmin/vistas/ajax/buscar_wallet.php
@@ -162,7 +162,7 @@ if ($dominio_actual == 'marketplace.imporsuit') {
                         ?>
 
                             <tr>
-                                <td class="text-center"> <a href="pagar_wallet.php?tienda=<?php echo $tienda ?>"> <?php echo $tienda; ?></a></td>
+                                <td class="text-center"> <a href="pagar_wallet.php?id_factura=&tienda=<?php echo $tienda ?>"> <?php echo $tienda; ?></a></td>
                                 <td class="text-center"><?php echo $simbolo_moneda . $total_venta; ?></td>
                                 <td class="text-center"><?php echo $simbolo_moneda . $total_pendiente; ?></td>
                                 <td class="text-center"><?php echo $simbolo_moneda . $monto_recibir; ?></td>
@@ -176,7 +176,7 @@ if ($dominio_actual == 'marketplace.imporsuit') {
                                             if ($permisos_eliminar == 1) { ?>
                                                 <!--<a class="dropdown-item" href="#" data-toggle="modal" data-target="#dataDelete" data-id="<?php echo $row['id_factura']; ?>"><i class='fa fa-trash'></i> Eliminar</a>-->
                                             <?php } ?>
-                                            <a href="pagar_wallet.php?tienda=<?php echo $tienda ?>" class="dropdown-item"> <i class="ti-wallet"></i> Pagar </a>
+                                            <a href="pagar_wallet.php?id_factura=&tienda=<?php echo $tienda ?>" class="dropdown-item"> <i class="ti-wallet"></i> Pagar </a>
 
                                         </div>
 
